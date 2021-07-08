2ca5f749fe7383556317b2f170917b5610766127
Jesse Hazel <jwhazel@me.com>
2021-07-08 19:01:40 -0400

    Refactor to use state manager

---
+++ store/community.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  title: 'Curabitur estes los este peolse pretium.',
+  subhead:
+    'Lorem ipsum dolor sit amet, conse ctetur adipiscing elit lirm ipsum estes son etes. Ipsum dolor sit amet, conse ctetur adipis.',
+  price: '4000',
+})
+
